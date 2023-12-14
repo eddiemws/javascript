db8a9332cb608c8c1aa470e239ada6f6a524b2a6
eddy <106661971+eddiemws@users.noreply.github.com>
2023-12-15 00:16:46 +0300

    Update trialjs.js

--- trialjs.js
+++ trialjs.js
@@ -5,6 +5,13 @@ function myFunction () {
     // document.getElementById("demo").innerHTML= 'This is a Javascript document'
 }
 
+function myFunction2 () {
+    document.getElementById("first").style.backgroundColor = 'black';
+    document.getElementById("second").style.color = 'red';
+    document.getElementById("third").style.color = 'blue';
+    document.getElementById("forth").style.color = 'red';
+}
+
 
 
 function Write () {
