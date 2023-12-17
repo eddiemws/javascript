ef2cad4948575436ca410b63712c05a83e561635
eddy <106661971+eddiemws@users.noreply.github.com>
2023-12-17 23:30:50 +0300

    Update trialjs.js

--- trialjs.js
+++ trialjs.js
@@ -66,13 +66,7 @@ let a, b, c;
 
 
 //function//
- function Operators () {
-    let text1 = 20;
-    let text2 = 5;
-    text2 +=1000;
-    results = text1 + text2;
-    document.getElementById("sixth").innerHTML = results;
- }
+
 
 
  // funtion type two. //
